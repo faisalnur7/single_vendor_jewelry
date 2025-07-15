6ce24497a3de4257a3f5fe65d8c5f076b3c71ca2
faisalnur7 <md.faisalnur55@gmail.com>
2025-07-15 21:11:55 +0600

    Renamed app name

--- resources/views/homepage.blade copy.php
+++ resources/views/homepage.blade copy.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Right BD</title>
+    <title>Stainless Steel Jewelry</title>
     <meta name="description" content="Free open source Tailwind CSS Store template">
     <meta name="keywords" content="">
 
