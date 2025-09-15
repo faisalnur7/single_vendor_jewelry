1d85a03ef29fa291dc6870942419a014acbc6693
faisalnur7 <md.faisalnur55@gmail.com>
2025-09-15 19:30:27 +0600

    Static Product details added

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -35,8 +35,8 @@
 
     <section class="mt-0 md:mt-8 py-6 px-6 min-h-[600px] bg-cover bg-right md:bg-center bg-no-repeat" style="background-image: url('/assets/img/images/about_stainless.webp')">
         <div class="flex w-full bg-transparent py-6 rounded">
-            <div class="w-1/2 hidden md:flex"></div>
-            <div class="w-full md:w-1/2">
+            <div class="w-1/2"></div>
+            <div class="w-1/2">
                 <div class="why_choose_us_list">
                 <h2 class="text-3xl md:text-5xl font-bold mb-6">Welcome to Stainless Steel Jewellery</h2>
                     {!! $homepage_setting->about !!}
